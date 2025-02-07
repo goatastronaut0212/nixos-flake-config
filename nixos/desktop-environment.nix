59e5242af439739ffd65cb7b407ad784e6fc5386
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-02-07 07:41:40 +0700

    Use gnome, awesome and hyprland in desktop environment

--- nixos/desktop-environment.nix
+++ nixos/desktop-environment.nix
@@ -1,8 +1,11 @@
 {
   services.xserver = {
-    desktopManager.cinnamon = {
+    desktopManager.gnome = {
       enable = true;
     };
-    displayManager.lightdm.enable = true;
+    displayManager.gdm.enable = true;
+    windowManager.awesome.enable = true;
   };
+  programs.hyprland.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
