bc036f955527734d3dc214b8d07a80805f0363de
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-03-01 07:47:00 +0700

    Delete all desktop environment only use tty

--- nixos/desktop-environment.nix
+++ nixos/desktop-environment.nix
@@ -1,10 +1,6 @@
 {
   services.xserver = {
-    desktopManager.gnome = {
-      enable = true;
-    };
-    displayManager.gdm.enable = true;
-    windowManager.awesome.enable = true;
+    displayManager.startx.enable = true;
   };
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
