3f176e8069e5401e3c0c1e19b3a08dd700e923a7
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-04-14 22:56:13 +0700

    Use plasma 6 desktop

--- nixos/desktop-environment.nix
+++ nixos/desktop-environment.nix
@@ -2,6 +2,9 @@
   services.xserver = {
     displayManager.startx.enable = true;
   };
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
   programs.hyprland.enable = true;
+  programs.sway.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
