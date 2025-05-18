650f8dafb4ff91c2d7e51cbec982a6a378d69048
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-05-18 15:03:56 +0700

    Update use hyprland window manager instead

--- nixos/desktop-environment.nix
+++ nixos/desktop-environment.nix
@@ -2,9 +2,4 @@
   services.xserver = {
     displayManager.startx.enable = true;
   };
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  programs.hyprland.enable = true;
-  programs.sway.enable = true;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
