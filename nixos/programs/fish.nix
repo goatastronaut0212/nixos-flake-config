c851ef8df2830e8854ad88a955e480a57e4506b5
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-07-15 19:57:50 +0700

    Update fish for yazi_files

--- nixos/programs/fish.nix
+++ nixos/programs/fish.nix
@@ -3,9 +3,22 @@
     enable = true;
     shellInit = ''
       function fish_prompt
-        #powerline-go -error $status -jobs (count (jobs -p)) -newline -cwd-max-depth 4
-        set -l prompt (powerline-go -error $status -jobs (count (jobs -p)) -newline -cwd-max-depth 4)
+        set -l prompt (powerline-go -error $status -jobs (count (jobs -p)) -cwd-max-depth 4)
         printf "%s\n" $prompt
+      end
+
+      function yazi_fm
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+          builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+      end
+
+      # Launch Yazi in interactive sessions
+      if status is-interactive
+        yazi_fm
       end
     '';
     shellAliases = {
